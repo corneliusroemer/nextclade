2ceac935ba91fa2dd0998d8b3805cdfc0af09a69
corneliusroemer <corneliusroemer@users.noreply.github.com>
2025-05-04 14:02:11 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/_next/static/chunks/972-01b1684264bfcb65.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[972],{20619:function(c,b,a){a.d(b,{CSE:function(){return e}});var d=a(79015);function e(a){return(0,d.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M955.7 856l-416-720c-6.2-10.7-16.9-16-27.7-16s-21.6 5.3-27.7 16l-416 720C56 877.4 71.4 904 96 904h832c24.6 0 40-26.6 27.7-48zM480 416c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v184c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V416zm32 352a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"}}]})(a)}}}])
+//# sourceMappingURL=972-01b1684264bfcb65.js.map
